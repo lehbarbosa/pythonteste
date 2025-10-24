77cef4679b73062ef571c71b54594e1bce5c6c01
Leandro Barbosa <leandrobarbosa1705@gmail.com>
2025-10-23 21:05:18 -0300

    Manipulação de string

---
+++ aula09.py
@@ -0,0 +1,30 @@
+frase = "Curso em Vídeo Python"
+# print(frase)
+# print(frase[3:13])
+# print(frase[:13])
+# print(frase[13:])
+# print(frase[1:15])
+# print(frase[1:15:2])
+# print(frase[1::2])
+# print(frase[::2])
+# print("""Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam id iste sunt exercitationem 
+# consequatur modi ipsa ullam aliquam vel aperiam. Optio, officia necessitatibus eos quod incidunt 
+# aliquam eius nihil ea!""")
+# print(frase.count('o'))
+# print(frase.upper().count('O'))
+# print(len(frase))
+# frase.replace('Python','Android')
+# print(frase)
+# frase = frase.replace('Python','Android')
+# print('Curso' in frase)
+# print(frase.find('Curso'))
+# print(frase.find('Vídeo'))
+# print(frase.find('vídeo'))
+# print(frase.lower().find('vídeo'))
+# dividido = frase.split()
+# print(dividido[0])
+# print(dividido[2][3])
+ 
+
+
+
