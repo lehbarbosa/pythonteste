653c7683b5858365cc0071e6bc023955eb5173b3
Leh Barbosa <leandrobarbosa1705@gmail.com>
2026-01-06 21:43:32 -0300

    .

---
+++ aula09.py
@@ -0,0 +1,28 @@
+frase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(dividido[0])
+print(dividido[2][3])
+
+
+# print(frase)
+# print(frase[3])
+# print(frase[3:13])
+# print(frase[:13])
+# print(frase[1:15])
+# print(frase[1:15:2])
+# print(frase[1::2])
+# print(frase[::2])
+# print("""Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+# Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+# when an unknown printer took a galley of type and scrambled it to make a type specimen book.""")
+
+# print(frase.count('o'))
+# print(frase.count('O'))
+# print(frase.upper().count('O'))
+# print(len(frase))
+# print(len(frase.strip()))
+# print(frase.replace('Python', 'Android'))
+# print('Curso' in frase)
+# print(frase.find('Curso'))
+# print(frase.find('vídeo'))
+# print(frase.lower().find('vídeo'))
